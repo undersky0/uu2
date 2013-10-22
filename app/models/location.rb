e9953f29cf464385a37d6f964c7f924af8cbc6b1
Richar <richardlonesteen@gmail.com>
2013-10-22 04:03:10 +0100

    d

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,12 @@
 class Location < ActiveRecord::Base
+  
+  acts_as_gmappable
+  
   attr_accessible :address, :gmaps, :latitude, :longitude, :postcode
+  
+  def gmaps4rails_address
+    address
+  end
+  
+  
 end
